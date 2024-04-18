aaa63ee682c96d122ae23be8f25fac56eaac1744
Anshul Bhartiya <126051112+bhartiyaanshul@users.noreply.github.com>
2024-04-18 14:59:22 +0530

    apis updated

--- Day-3/apis.js
+++ Day-3/apis.js
@@ -4,21 +4,8 @@ const app = express()
 app.use(express.json());
 const port = 3000
 
-// app.post('/',(req,res)=>{
-//     const {name, surename, phone, address} = req.query
-//     res.send(name+surename+phone+address)
-//     const content = {
-//         name: name,
-//         surename: surename,
-//         phone: phone,
-//         address: address
-//     }
-//     fs.writeFileSync('./output.json',JSON.stringify(content,null,4))
-// })
-
 app.post('/',(req,res)=>{
-    const {name, surename, phone, address} = req.body
-    console.log(req.body)
+    const {name, surename, phone, address} = req.query
     res.send(name+surename+phone+address)
     const content = {
         name: name,
@@ -29,7 +16,32 @@ app.post('/',(req,res)=>{
     fs.writeFileSync('./output.json',JSON.stringify(content,null,4))
 })
 
-app.get('/',(req,res)=>{
+app.post('/', (req, res) => {
+    const { name, surename, phone, address } = req.body
+    console.log(req.body)
+    res.send(name + surename + phone + address)
+    const content = {
+        name: name,
+        surename: surename,
+        phone: phone,
+        address: address
+    }
+
+    const fileExists = fs.existsSync('./output.json')
+    if(fileExists){
+        const data = fs.readFileSync('./output.json')
+        if (data) {
+            const parsedData = JSON.parse(data)
+            parsedData.push(content)
+            fs.writeFileSync('./output.json', JSON.stringify(parsedData, null, 4))
+        }
+    } else {
+        const parsedContent = [content]
+        fs.writeFileSync('./output.json', JSON.stringify(parsedContent, null, 4))
+    }
+})
+
+app.get('/', (req, res) => {
 
     // STRING REPONSE
 
@@ -38,10 +50,10 @@ app.get('/',(req,res)=>{
     // res.send(`Name: ${name} \n Surename: ${surename} \n Phone: ${phone} \n Address: ${address}`)
 
     // JSON RESPONSE
-    const data = fs.readFileSync('./output.json','utf8')
+    const data = fs.readFileSync('./output.json', 'utf8')
     res.send(data)
 })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-  })
+})
